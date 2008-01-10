f83c0906c8dc6422dc419b2c5c489d257ec00b54
Gabriel Levy <gabelevy@gmail.com>
2008-01-10 08:17:07 +0000

    Added functions to WindowsService to start and stop services through the SCM. Added start call to install, added stop cal to uninstall, changed install type to auto-start
    git-svn-id: svn+ssh://localhost/home/svn/musador/trunk@17 abf014a6-d86a-7740-8175-6a5112e866a6

--- src/Utilities/WindowsService.h
+++ src/Utilities/WindowsService.h
@@ -6,6 +6,7 @@
 #include <windows.h>
 #include <tchar.h>
 #include "Singleton.h"
+#include "Util.h"
 #include "StreamException.h"
 
 #if defined(UNICODE) || defined(_UNICODE)
@@ -31,6 +32,10 @@ public:
 
 	void stop();
 
+	void launch();
+
+	void terminate();
+
 	static void WINAPI runThunk(DWORD argc, LPTSTR argv[])
 	{
 		T::instance()->_run(argc, argv);
@@ -54,6 +59,7 @@ private:
 	SERVICE_STATUS status;
 	HANDLE evtStop;
 	HANDLE evtSrc;
+	SC_HANDLE hSvc;
 
 	void _run(DWORD argc, LPTSTR argv[]);
 	void _ctrl(DWORD opCode);
@@ -73,7 +79,8 @@ public:
 
 template <class T>
 WindowsService<T>::WindowsService(const tstring& svcName) :
-svcName(svcName)
+svcName(svcName),
+hSvc(NULL)
 {
 	this->evtStop = ::CreateEvent(NULL,TRUE,FALSE,NULL);
 	this->evtSrc = ::RegisterEventSource(NULL,this->svcName.c_str());
@@ -90,7 +97,6 @@ template <class T>
 void WindowsService<T>::install()
 {
     SC_HANDLE schSCManager;
-    SC_HANDLE schService;
     TCHAR szPath[MAX_PATH];
 
     if( !GetModuleFileName( NULL, szPath, MAX_PATH ) )
@@ -110,13 +116,13 @@ void WindowsService<T>::install()
     }
 
     // Create the service
-	schService = ::CreateService( 
+	this->hSvc = ::CreateService( 
         schSCManager,              // SCM database 
         this->svcName.c_str(),     // name of service 
         this->svcName.c_str(),		// service name to display 
         SERVICE_ALL_ACCESS,        // desired access 
         SERVICE_WIN32_OWN_PROCESS, // service type 
-        SERVICE_DEMAND_START,      // start type 
+        SERVICE_AUTO_START,      // start type 
         SERVICE_ERROR_NORMAL,      // error control type 
         szPath,                    // path to service's binary 
         NULL,                      // no load ordering group 
@@ -125,13 +131,25 @@ void WindowsService<T>::install()
         NULL,                      // LocalSystem account 
         NULL);                     // no password 
  
-    if (schService == NULL) 
+	
+    if (this->hSvc == NULL) 
     {
 		::CloseServiceHandle(schSCManager);
 		throw ServiceException() << "CreateService failed: " << ::GetLastError();
     }
+	
+	// Start the service
+	try
+	{
+		this->launch();
+	}
+	catch (const ServiceException& e)
+	{
+		this->logEvent(EVENTLOG_INFORMATION_TYPE,Util::utf8ToUnicode(e.what()));
+	}
 
-	::CloseServiceHandle(schService); 
+	
+	::CloseServiceHandle(this->hSvc); 
 	::CloseServiceHandle(schSCManager);
 }
 
@@ -139,7 +157,6 @@ template <class T>
 void WindowsService<T>::uninstall()
 {
     SC_HANDLE schSCManager;
-    SC_HANDLE schService;
     TCHAR szPath[MAX_PATH];
 
     if( !GetModuleFileName( NULL, szPath, MAX_PATH ) )
@@ -159,20 +176,30 @@ void WindowsService<T>::uninstall()
     }
 
     // Delete the service
-	schService = ::OpenService( 
+	this->hSvc = ::OpenService( 
         schSCManager,           // SCM database 
         this->svcName.c_str(),  // name of service 
-		DELETE);				// DELETE access
+		DELETE | SERVICE_STOP );				// access
 
-    if (schService == NULL) 
+    if (this->hSvc == NULL) 
     {
 		::CloseServiceHandle(schSCManager);
 		throw ServiceException() << "The specified service is not installed. (" << ::GetLastError() << ")";
     }
 
-	BOOL r = ::DeleteService(schService);
+	// Stop the service
+	try
+	{
+		this->terminate();
+	}
+	catch (const ServiceException& e)
+	{
+		this->logEvent(EVENTLOG_INFORMATION_TYPE,Util::utf8ToUnicode(e.what()));
+	}
 
-	::CloseServiceHandle(schService); 
+	BOOL r = ::DeleteService(this->hSvc);
+
+	::CloseServiceHandle(this->hSvc); 
 	::CloseServiceHandle(schSCManager);
 
 	if (!r)
@@ -203,6 +230,27 @@ template <class T>
 void WindowsService<T>::stop()
 {
 	::SetEvent(this->evtStop);
+}
+
+template <class T>
+void WindowsService<T>::launch()
+{
+	// Start the Service through the SCM
+	if (0 == ::StartService(this->hSvc, 0, NULL))
+	{
+		throw ServiceException() << "The service could not be started. (" << ::GetLastError() << ")";
+	}
+}
+
+template <class T>
+void WindowsService<T>::terminate()
+{
+	// Stop the Service through the SCM
+	SERVICE_STATUS st;
+	if (0 == ::ControlService(this->hSvc, SERVICE_CONTROL_STOP, &st))
+	{
+		throw ServiceException() << "The service could not be stopped. (" << ::GetLastError() << ")";
+	}
 }
 
 template <class T>
