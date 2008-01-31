54acf8289c60824881d86dd5d0bd23350e7b6cfe
Gabriel Levy <gabelevy@gmail.com>
2008-01-31 03:49:08 +0000

    Split Boost projects and dependencies
    git-svn-id: svn+ssh://localhost/home/svn/musador/trunk@27 abf014a6-d86a-7740-8175-6a5112e866a6

--- src/Server/NullConnection.h
+++ src/Server/NullConnection.h
@@ -1,6 +1,10 @@
 #ifndef NULL_CONNECTION_H_66E5DEFA_BA19_45d1_8990_2F5FBD85F9DD
 #define NULL_CONNECTION_H_66E5DEFA_BA19_45d1_8990_2F5FBD85F9DD
 
+#include "Connection.h"
+
+using namespace Musador;
+
 class NullConnection : public Connection
 {
 	void accepted() { this->beginRead(); }
