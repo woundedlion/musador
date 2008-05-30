68f2e4fc88f0e001767815ace784090e5bb085ba
Gabriel Levy <gabelevy@gmail.com>
2008-05-30 04:19:46 +0000

    Added docs for the Config headers
    git-svn-id: svn+ssh://localhost/home/svn/musador/trunk@84 abf014a6-d86a-7740-8175-6a5112e866a6

--- src/Config/Config.h
+++ src/Config/Config.h
@@ -10,74 +10,110 @@
 namespace Musador
 {
 
-	class HTTPConfig
-	{
-	public:
-		
-		HTTPConfig() :
-			documentRoot(),
-			addr("0.0.0.0"),
-			port(5152),
-			requireAuth(false),
-			realm(L"")
-		{}
-	
-		void addUser(HTTP::User& u);
-		
-		template<class Archive>
-		void serialize(Archive & ar, const unsigned int version)
-		{
-			ar & BOOST_SERIALIZATION_NVP(documentRoot);
-			ar & BOOST_SERIALIZATION_NVP(addr);
-			ar & BOOST_SERIALIZATION_NVP(port);
-			ar & BOOST_SERIALIZATION_NVP(requireAuth);
-			ar & BOOST_SERIALIZATION_NVP(users);
-			ar & BOOST_SERIALIZATION_NVP(realm);
-		}
+    /// @class Contains configuration data specific to an HTTP site
+    class HTTPConfig
+    {
+    public:
+        
+        /// Constructor
+        HTTPConfig() :
+            documentRoot(),
+            addr("0.0.0.0"),
+            port(5152),
+            requireAuth(false),
+            realm(L"")
+        {}
+    
+        /// Add an HTTP user to the configuration.
+        /// @param[in] user User information for the new user to add.
+        void addUser(const HTTP::User& user);
+        
+        /// Serialize this configuration.
+        /// @param[in] ar Destination archive for the serialized data.
+        /// @param[in] version The version of the archive. Used by boost::serailization version tracking.
+        template<class Archive>
+        void serialize(Archive & ar, const unsigned int version)
+        {
+            ar & BOOST_SERIALIZATION_NVP(documentRoot);
+            ar & BOOST_SERIALIZATION_NVP(addr);
+            ar & BOOST_SERIALIZATION_NVP(port);
+            ar & BOOST_SERIALIZATION_NVP(requireAuth);
+            ar & BOOST_SERIALIZATION_NVP(users);
+            ar & BOOST_SERIALIZATION_NVP(realm);
+        }
 
-		std::wstring documentRoot;
-		std::string addr;
-		unsigned short port;
-		bool requireAuth;
-		HTTP::UserCollection users;
-		std::wstring realm;
-	};
+        /// The root directory for the site, available at http://addr:port/
+        std::wstring documentRoot;
 
-	class Controller;
+        /// The IP Address in dot notation on which to listen for requests to this site.
+        std::string addr;
 
-	class ServerConfig
-	{
-	public:
-		
-		typedef std::vector<HTTPConfig> HTTPSiteCollection;
+        /// The port number on which to listen for requests to this site.
+        unsigned short port;
 
-		template<class Archive>
-		void serialize(Archive & ar, const unsigned int version)
-		{
-			ar & BOOST_SERIALIZATION_NVP(sites);
-		}
+        /// Specifies whether HTTP Digest authentication is required to access this site.
+        bool requireAuth;
 
-		HTTPSiteCollection sites;
+        /// Collection of users with credentials to access the site.
+        HTTP::UserCollection users;
 
-		/************************/
-		Controller * controller;
-	};
+        /// Realm name used for HTTP Authentication
+        std::wstring realm;
 
-	class Config : public Singleton<Config>
-	{
-	public:
+    };
 
-		bool load(const std::wstring& path);
-		bool save(const std::wstring& path);
+    class Controller;
 
-		template<class Archive>
-		void serialize(Archive & ar, const unsigned int version)
-		{
-			ar & BOOST_SERIALIZATION_NVP(server);
-		}
-		
-		ServerConfig server;
-	};
+    /// @class Contains configuration data specific to an HTTP server.
+    class ServerConfig
+    {
+    public:
+        
+        /// Collection type for HTTPConfig instances.
+        typedef std::vector<HTTPConfig> HTTPSiteCollection;
+
+        /// Serialize this configuration.
+        /// @param[in] ar Destination archive for the serialized data.
+        /// @param[in] version The version of the archive. Used by boost::serailization version tracking.
+       template<class Archive>
+        void serialize(Archive & ar, const unsigned int version)
+        {
+            ar & BOOST_SERIALIZATION_NVP(sites);
+        }
+
+        /// Collection of the sites defined for this server.
+        HTTPSiteCollection sites;
+
+        /// Used inernally. NOT SERIALIZED.
+        /// Pointer to the controller instance which implements application-specific logic
+        Controller * controller;
+    };
+
+    /// @class Top-level configuration container. Contains the configuration for the entire system.
+    class Config : public Singleton<Config>
+    {
+    public:
+
+        /// Load the configuration data contained in the specified file.
+        /// @param[in] The path to the config file
+        bool load(const std::wstring& path);
+
+        /// Save the configuration data to specified file.
+        /// @param[in] The path to the destination file.
+        bool save(const std::wstring& path);
+
+        /// Serialize this configuration.
+        /// @param[in] ar Destination archive for the serialized data.
+        /// @param[in] version The version of the archive. Used by boost::serailization version tracking.
+        template<class Archive>
+        void serialize(Archive & ar, const unsigned int version)
+        {
+            ar & BOOST_SERIALIZATION_NVP(server);
+        }
+        
+        /// The Server configuration for this application
+        ServerConfig server;
+    };
 
 
 }
