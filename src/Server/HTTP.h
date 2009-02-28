b7fd644ee4f8475f3d949def3b151d0f22d34ea1
Gabriel Levy <gabelevy@gmail.com>
2009-02-28 01:17:14 +0000

    - Added docs for HTTP.h
    git-svn-id: svn+ssh://localhost/home/svn/musador/trunk@144 abf014a6-d86a-7740-8175-6a5112e866a6

--- src/Server/HTTP.h
+++ src/Server/HTTP.h
@@ -34,25 +34,38 @@ namespace Musador
     namespace HTTP
     {
 
-        //////////////////////////////////////////////////////////////////////
-        /// User
-        //////////////////////////////////////////////////////////////////////
+        /// @class User
+        /// @brief Object representing a web user
         class User
         {
         public:
 
+            /// @brief Default Constructor
             User();
 
+            /// @brief Constructor
+            /// @param[in] username The username of the newly constructed User
             User(const std::string& username);
 
+            /// @brief Get this User's username
+            /// @retuns The User's username
             std::string getUsername() const;
 
+            /// @brief Set this User's username
+            /// @param[in] The username to set for this User
             void setUsername(std::string& username);
 
+            /// @brief Get this User's password
+            /// @retuns The User's password
             std::string getPassword() const;
 
+            /// @brief Set this User's password
+            /// @param[in] The password to set for this User
             void setPassword(const std::string& password);
 
+            /// @brief Serialize this User to XML
+            /// @param[in] ar Destination archive for the serialized data.
+            /// @param[in] version The version of the archive. Used by boost::serialization version tracking.
             template<class Archive>
             void serialize(Archive & ar, const unsigned int version)
             {
@@ -67,38 +80,70 @@ namespace Musador
 
         };
 
+        /// @brief Collection type for HTTP Users
         typedef std::map<std::string,User> UserCollection;	
+
+        /// @brief Collection type for HTTP headers
         typedef std::map<std::string,std::string> HeaderCollection;
+
+        /// @brief Collection type for HTTP querystring parameters
         typedef std::map<std::string,std::string> ParamCollection;
+
+        /// @brief Collection type for HTTP Cookies
         typedef std::map<std::string,std::string> CookieCollection;	
 
-        //////////////////////////////////////////////////////////////////////
-        /// Cookie free functions
-        //////////////////////////////////////////////////////////////////////
-        void parseCookie(const std::string& cookieStr, CookieCollection& cookies);
-
-        //////////////////////////////////////////////////////////////////////
-        /// Request
-        //////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////
+        
+        /// @class Request
+        /// @brief An HTTP Request
         class Request 
         {
         public:
 
+            /// @brief Constructor
             Request();
+
+            /// @brief Destructor
             ~Request();
 
+            /// @brief Reset this Request to an empty state, as if newly constructed
             void clear();
+
+            /// @brief Send the request headers over the specified Connection
+            /// @param[in] conn The IO:Connection object on which the header data is sent
             void sendHeaders(IO::Connection& conn);
+
+            /// @brief Send the Request body over the specified Connection
+            /// @param[in] conn The IO:Connection object on which the request data is sent
             void sendBody(IO::Connection& conn);
+            
+            /// @brief Dump an HTML representation of the Request to the spcified output stream
+            /// Useful for debugging
             void dump(std::ostream& info);
 
+            /// @brief the URI portion of the request URL
             std::string requestURI;
+
+            /// @brief the query string portion of the request URL
             std::string queryString;
+
+            /// @brief The HTTP protocol and version specified in the request e.g. HTTP/1.1
             std::string protocol;
+
+            /// @brief The HTTP request method e.g. GET, POST, HEAD, OPTIONS, PUT, DELETE
             std::string method;
+
+            /// @brief The key-value parameter pairs passed to the request in the query string (int the case of a GET)
+            /// or in the body (in the case of a POST)
             ParamCollection params;
+
+            /// @brief The header name-value pairs in the request
             HeaderCollection headers;
+
+            /// The cookies name-value pairs in the request
             CookieCollection cookies;
+
+            /// A stream containing the request data (body of the request)
             boost::shared_ptr<std::iostream> data;
 
         };
@@ -150,6 +195,8 @@ namespace Musador
         //////////////////////////////////////////////////////////////////////
         /// Free functions
         //////////////////////////////////////////////////////////////////////
+
+        void parseCookie(const std::string& cookieStr, CookieCollection& cookies);
 
         std::string getRFC1123(const time_t& timer);
 
